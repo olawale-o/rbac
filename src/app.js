8614126e177a98e746b1dca34e5c93544aca250b
olawale-o <omoogunolawale@gmail.com>
2025-01-12 11:52:10 +0100

    Remove the use of cors

--- src/app.js
+++ src/app.js
@@ -1,14 +1,12 @@
 const express = require("express");
 const cookieParser = require("cookie-parser");
-const cors = require("cors");
-const errorHandler = require("../middleware/errorHandler");
+const errorHandler = require("../middlewares/error");
 
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(cors());
 
 app.use("/api/v1/auth", require("../api/auth"));
 app.use("/api/v1/engineering", require("../api/engineering"));

d0726b2c64178f113840280dea8959e83d588e17
olawale-o <omoogunolawale@gmail.com>
2025-01-13 11:55:21 +0100

    Create checkRole middleware

---
+++ middlewares/role.js
@@ -0,0 +1,17 @@
+const checkRole = (roleName) => {
+  return async (req, res, next) => {
+    const { user } = req.cookies;
+    const userRole = user ? user.roles : "anonymous";
+    const inRole = userRole.find((role) => role.name === roleName);
+    if (inRole) {
+      req.permissions = inRole.Permissions;
+      return next();
+    } else {
+      return res
+        .status(403)
+        .json({ error: "Access denied, You dont have enough role" });
+    }
+  };
+};
+
+module.exports = { checkRole };

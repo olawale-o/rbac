1c202b2eefc0bbdae5b63a76fa99f77e513a0510
olawale-o <omoogunolawale@gmail.com>
2025-01-13 11:52:35 +0100

    Update admin resource

--- api/admin/index.js
+++ api/admin/index.js
@@ -1,3 +1,8 @@
 const router = require("express").Router();
+const handler = require("./handler");
+
+router.get("/", async (req, res, next) => {
+  return res.status(200).json({ message: "Admin resource" });
+});
 
 module.exports = router;

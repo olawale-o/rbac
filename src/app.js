2be9151701df5d118f18f26b1797e7936e128c29
olawale-o <omoogunolawale@gmail.com>
2025-01-13 11:51:15 +0100

    Update server

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const cookieParser = require("cookie-parser");
 const errorHandler = require("../middlewares/error");
+const { checkGroup } = require("../middlewares/group");
 
 const app = express();
 
@@ -9,9 +10,14 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 
 app.use("/api/v1/auth", require("../api/auth"));
-app.use("/api/v1/engineering", require("../api/engineering"));
-app.use("/api/v1/sales", require("../api/sales"));
-app.use("/api/v1/admin", require("../api/admin"));
+app.use("/api/v1/users", require("../api/users"));
+app.use(
+  "/api/v1/engineering",
+  checkGroup("Engineering"),
+  require("../api/engineering"),
+);
+app.use("/api/v1/sales", checkGroup("Sales"), require("../api/sales"));
+app.use("/api/v1/admin", checkGroup("Admin"), require("../api/admin"));
 app.use(errorHandler);
 
 module.exports = app;

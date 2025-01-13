203373b8492db46a52a555ca18314a388aeba90d
olawale-o <omoogunolawale@gmail.com>
2025-01-13 11:53:25 +0100

    Update sales resource

--- api/sales/index.js
+++ api/sales/index.js
@@ -1,3 +1,7 @@
 const router = require("express").Router();
 
+router.get("/", async (req, res, next) => {
+  return res.status(200).json({ message: "Sales resource" });
+});
+
 module.exports = router;

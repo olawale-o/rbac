24825a132e790d15e2b4859de8978d07b642c499
olawale-o <omoogunolawale@gmail.com>
2025-01-13 17:28:41 +0100

    Update seeders

--- seeders/20250112040717-demo-role-group-ermission.js
+++ seeders/20250112040717-demo-role-group-ermission.js
@@ -12,6 +12,131 @@ module.exports = {
      *   isBetaMember: false
      * }], {});
      */
+    // await queryInterface.bulkInsert(
+    //   "roles_groups_permissions",
+    //   [
+    //     {
+    //       permission_id: 1,
+    //       permittable_id: 1,
+    //       permittable_type: "group",
+    //       createdAt: new Date(),
+    //       updatedAt: new Date(),
+    //     },
+    //     {
+    //       permission_id: 2,
+    //       permittable_id: 1,
+    //       permittable_type: "role",
+    //       createdAt: new Date(),
+    //       updatedAt: new Date(),
+    //     },
+    //     {
+    //       permission_id: 3,
+    //       permittable_id: 1,
+    //       permittable_type: "role",
+    //       createdAt: new Date(),
+    //       updatedAt: new Date(),
+    //     },
+    //     {
+    //       permission_id: 4,
+    //       permittable_id: 1,
+    //       permittable_type: "role",
+    //       createdAt: new Date(),
+    //       updatedAt: new Date(),
+    //     },
+    //     {
+    //       permission_id: 5,
+    //       permittable_id: 2,
+    //       permittable_type: "group",
+    //       createdAt: new Date(),
+    //       updatedAt: new Date(),
+    //     },
+    //     {
+    //       permission_id: 6,
+    //       permittable_id: 2,
+    //       permittable_type: "role",
+    //       createdAt: new Date(),
+    //       updatedAt: new Date(),
+    //     },
+    //     {
+    //       permission_id: 7,
+    //       permittable_id: 2,
+    //       permittable_type: "role",
+    //       createdAt: new Date(),
+    //       updatedAt: new Date(),
+    //     },
+    //     {
+    //       permission_id: 8,
+    //       permittable_id: 2,
+    //       permittable_type: "role",
+    //       createdAt: new Date(),
+    //       updatedAt: new Date(),
+    //     },
+    //     {
+    //       permission_id: 9,
+    //       permittable_id: 3,
+    //       permittable_type: "group",
+    //       createdAt: new Date(),
+    //       updatedAt: new Date(),
+    //     },
+    //     {
+    //       permission_id: 10,
+    //       permittable_id: 3,
+    //       permittable_type: "role",
+    //       createdAt: new Date(),
+    //       updatedAt: new Date(),
+    //     },
+    //     {
+    //       permission_id: 11,
+    //       permittable_id: 3,
+    //       permittable_type: "role",
+    //       createdAt: new Date(),
+    //       updatedAt: new Date(),
+    //     },
+    //     {
+    //       permission_id: 12,
+    //       permittable_id: 3,
+    //       permittable_type: "role",
+    //       createdAt: new Date(),
+    //       updatedAt: new Date(),
+    //     },
+    //     {
+    //       permission_id: 13,
+    //       permittable_id: 3,
+    //       permittable_type: "role",
+    //       createdAt: new Date(),
+    //       updatedAt: new Date(),
+    //     },
+    //     {
+    //       permission_id: 14,
+    //       permittable_id: 4,
+    //       permittable_type: "role",
+    //       createdAt: new Date(),
+    //       updatedAt: new Date(),
+    //     },
+    //     {
+    //       permission_id: 15,
+    //       permittable_id: 4,
+    //       permittable_type: "role",
+    //       createdAt: new Date(),
+    //       updatedAt: new Date(),
+    //     },
+    //     {
+    //       permission_id: 16,
+    //       permittable_id: 4,
+    //       permittable_type: "role",
+    //       createdAt: new Date(),
+    //       updatedAt: new Date(),
+    //     },
+    //     {
+    //       permission_id: 17,
+    //       permittable_id: 4,
+    //       permittable_type: "role",
+    //       createdAt: new Date(),
+    //       updatedAt: new Date(),
+    //     },
+    //   ],
+    //   {},
+    // );
     await queryInterface.bulkInsert(
       "roles_groups_permissions",
       [

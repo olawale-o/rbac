87b9056855f3cf94f13da377a2f71a3500d066a4
olawale-o <omoogunolawale@gmail.com>
2025-01-13 17:30:39 +0100

    Update database config

--- config/config.js
+++ config/config.js
@@ -2,7 +2,7 @@ module.exports = {
   development: {
     username: "postgres",
     password: null,
-    database: "rbac_development",
+    database: "bac_development",
     host: "127.0.0.1",
     dialect: "postgres",
     logging: false,

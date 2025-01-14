63dd923dacb45ac13b2f3a94a8dfda5dcdce6477
olawale-o <omoogunolawale@gmail.com>
2025-01-15 00:31:44 +0100

    Provide default port

--- index.js
+++ index.js
@@ -3,8 +3,8 @@ const http = require("http");
 const app = require("./src/app");
 const server = http.createServer(app);
 
-server.listen(parseInt(5000, 10), () => {
-  console.log("Server started on port 5000");
+server.listen(parseInt(process.env.PORT || 3000, 10), () => {
+  console.log("Server started on port " + process.env.PORT);
 });
 
 module.exports = server;

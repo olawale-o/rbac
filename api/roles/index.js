2cc4051312c44a7e3d240f78eda7f5e0bf54f576
olawale-o <omoogunolawale@gmail.com>
2025-01-31 16:29:36 +0100

    Update roles resource

--- api/roles/index.js
+++ api/roles/index.js
@@ -1,6 +1,10 @@
 const router = require("express").Router();
 const handler = require("./handler");
 
+router.get("/", async (req, res, next) => {});
+router.get("/:id", async (req, res, next) => {});
 router.post("/", async (req, res, next) => {});
+router.put("/:id", async (req, res, next) => {});
+router.delete("/:id", async (req, res, next) => {});
 
 module.exports = router;

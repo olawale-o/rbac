c81f642d42dc97903486d652af7e204201954695
olawale-o <omoogunolawale@gmail.com>
2025-05-02 10:34:17 +0100

    refactor: replace checkPermission with isAuthorized

--- middlewares/permission.js
+++ middlewares/permission.js
@@ -1,38 +1,3 @@
-const groupAccess = (groupPermissionType, groups) => {
-  const inGroup = groups.find((group) => {
-    return group.user_group_permission.find(
-      (permission) => permission.type === groupPermissionType,
-    );
-  });
-  if (inGroup) {
-    return true;
-  }
-  return false;
-};
-
-const checkPermission = (type) => {
-  return async (req, res, next) => {
-    const { user } = req.cookies;
-    const groupCanAccess = groupAccess(type, user.groups);
-    if (groupCanAccess) {
-      return next();
-    }
-    const userRoles = user ? user.roles : [];
-
-    isPermissionFound = userRoles.find((role) => {
-      return role.user_role_permission.Permissions.find(
-        (permission) => permission.type === type,
-      );
-    });
-    if (isPermissionFound) {
-      return next();
-    }
-    return res
-      .status(403)
-      .json({ error: "Access denied. You dont have enough permission" });
-  };
-};
-
 const isAuthorized = (permission) => {
   return async (req, res, next) => {
     const { user } = req.cookies;
@@ -46,4 +11,4 @@ const isAuthorized = (permission) => {
   };
 };
 
-module.exports = { checkPermission, isAuthorized };
+module.exports = { isAuthorized };

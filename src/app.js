54b68797c7c179898fdcf1cfc74de380da952911
olawale-o <omoogunolawale@gmail.com>
2025-05-02 10:35:31 +0100

    fix: imports

--- src/app.js
+++ src/app.js
@@ -1,8 +1,8 @@
 const express = require("express");
 const cookieParser = require("cookie-parser");
 const errorHandler = require("../middlewares/error");
-const { checkPermission } = require("../middlewares/permission");
-const { checkAuth } = require("../middlewares/auth");
+const { isAuthorized } = require("../middlewares/permission");
+const { isAuthenticated } = require("../middlewares/auth");
 
 const app = express();
 
@@ -12,16 +12,16 @@ app.use(cookieParser());
 
 app.use("/api/v1/auth", require("../api/auth"));
 
-app.use(checkAuth);
+app.use(isAuthenticated);
 app.use("/api/v1/users", require("../api/users"));
 app.use(
   "/api/v1/engineering",
-  checkPermission("can_view_engineering"),
+  isAuthorized("can_view_engineering"),
   require("../api/engineering"),
 );
 app.use(
   "/api/v1/sales",
-  checkPermission("can_view_sales"),
+  isAuthorized("can_view_sales"),
   require("../api/sales"),
 );
 app.use("/api/v1/admin", require("../api/admin"));

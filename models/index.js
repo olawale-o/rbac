9573e34660a2d1404ce7df14a6325eab050d0bc2
olawale-o <omoogunolawale@gmail.com>
2025-05-29 18:20:25 +0100

    refactor: sequelize generate model

--- models/index.js
+++ models/index.js
@@ -1,9 +1,7 @@
-"use strict";
-
-const fs = require("fs");
-const path = require("path");
+const fs = require("node:fs");
+const path = require("node:path");
 const Sequelize = require("sequelize");
-const process = require("process");
+const process = require("node:process");
 const basename = path.basename(__filename);
 const env = process.env.NODE_ENV || "development";
 const config = require(__dirname + "/../config/config.js")[env];

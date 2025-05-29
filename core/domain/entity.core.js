805e21ea99e913f4af73bc12bb7028a48851b64b
olawale-o <omoogunolawale@gmail.com>
2025-05-29 18:22:13 +0100

    refactor: entity.core.js

--- core/domain/entity.core.js
+++ core/domain/entity.core.js
@@ -1,3 +1,23 @@
-class Entity {}
+class Entity {
+  constructor({ id, createdAt, updatedAt, props }) {
+    const now = new Date();
+    this._id = id;
+    this._createdAt = createdAt || now;
+    this._updatedAt = updatedAt || now;
+    this.props = props;
+  }
+
+  get id() {
+    return this._id;
+  }
+
+  get createdAt() {
+    return this._createdAt;
+  }
+
+  get updatedAt() {
+    return this._updatedAt;
+  }
+}
 
 module.exports = { Entity };

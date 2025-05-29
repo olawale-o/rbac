63d65826635b776680fb474c67515b609e7f45bf
olawale-o <omoogunolawale@gmail.com>
2025-05-29 18:24:28 +0100

    update: repository constructor with props

--- core/repository/repository.core.js
+++ core/repository/repository.core.js
@@ -1,9 +1,27 @@
 class Repository {
+  constructor(sequelize, model) {
+    this.sequelize = sequelize;
+    this.model = model;
+  }
   async save(data) {
+    data.validate();
     // Implementation of saving data to the repository
+    // await this.model.create(data);
   }
   async findBy(obj) {
     // Implementation of finding data by ID from the repository
+  }
+
+  async transaction(callback) {
+    const transaction = await this.sequelize.transaction();
+    try {
+      await callback(transaction);
+      await transaction.commit();
+      console.log("Transaction completed");
+    } catch (error) {
+      await transaction.rollback();
+      throw error;
+    }
   }
 }
 

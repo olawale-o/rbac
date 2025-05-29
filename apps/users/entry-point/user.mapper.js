6d5ab034bdb6c0b49eb638a6b02136ec38fdd2f9
olawale-o <omoogunolawale@gmail.com>
2025-05-29 18:26:50 +0100

    feat: implement UserMapper

---
+++ apps/users/entry-point/user.mapper.js
@@ -0,0 +1,41 @@
+import { Mapper } from "core/domain/mapper.core";
+import User from "../domain/user.entity";
+class UserMapper extends Mapper {
+  toPersistence(entity) {
+    return {
+      id: entity.id,
+      email: entity.email,
+      fullName: entity.fullName,
+      password: entity.password,
+      createdAt: entity.createdAt,
+      updatedAt: entity.updatedAt,
+    };
+  }
+
+  toDomain(record) {
+    const entity = new User({
+      id,
+      createdAt: new Date(record.createdAt),
+      updatedAt: new Date(record.updatedAt),
+      props: {
+        email: record.email,
+        fullName: record.fullName,
+        password: record.password,
+      },
+    });
+
+    return entity;
+  }
+
+  toResponse(entity) {
+    return {
+      id: entity.id,
+      email: entity.email,
+      fullName: entity.fullName,
+      createdAt: entity.createdAt,
+      updatedAt: entity.updatedAt,
+    };
+  }
+}
+
+module.exports = { UserMapper };

6b9cf01a2625de1910fd5b7e5dea4558db8158cf
olawale-o <omoogunolawale@gmail.com>
2025-06-01 16:22:27 +0100

    fix: import error

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,17 +1,17 @@
-const AppError = require("../libraries/error/src");
 const { verifyToken } = require("../libraries/jwt/src");
+const { ForbiddenException } = require("../libraries/exception/exceptions");
 
 const isAuthenticated = async (req, res, next) => {
   const { accessToken } = req.cookies;
 
   if (!accessToken) {
-    throw new AppError(403, "Access token is required");
+    throw new ForbiddenException("Access token is required");
   }
 
   try {
     const data = verifyToken(accessToken, "ACCESS_TOKEN");
     if (!data) {
-      throw new AppError(403, "Access token is required");
+      throw new ForbiddenException("Access token is required");
     }
     req.data = data;
     next();

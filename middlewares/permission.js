9f702553a0dba2f6363a7fc7bd1acf230f107d4c
olawale-o <omoogunolawale@gmail.com>
2025-06-02 10:53:51 +0100

    refactor: throw exception from isAuthorized middleware

--- middlewares/permission.js
+++ middlewares/permission.js
@@ -1,13 +1,29 @@
+const { verifyToken } = require("../libraries/jwt/src");
+const { UnauthorizedException } = require("../libraries/exception/exceptions");
+
 const isAuthorized = (permission) => {
   return async (req, res, next) => {
-    const { user } = req.cookies;
-    const isPermissionFound = user.permissions.indexOf(permission) !== -1;
-    if (isPermissionFound) {
-      return next();
+    try {
+      if (!req.cookies) {
+        throw new UnauthorizedException("Cookies are required");
+      }
+
+      if (!req.cookies.accessToken) {
+        throw new UnauthorizedException("Cookies are required");
+      }
+
+      const { accessToken } = req.cookies;
+      const data = verifyToken(accessToken, "ACCESS_TOKEN");
+      const isPermissionFound = data.permissions.indexOf(permission) !== -1;
+      if (isPermissionFound) {
+        return next();
+      }
+      throw new UnauthorizedException(
+        "Access denied. You dont have enough permission",
+      );
+    } catch (error) {
+      next(error);
     }
-    return res
-      .status(403)
-      .json({ error: "Access denied. You dont have enough permission" });
   };
 };
 

97e50e534a62c7de09c4fa45d440b31784723429
olawale-o <omoogunolawale@gmail.com>
2025-06-08 16:05:45 +0100

    fix: error handling

--- middlewares/is-authenticated.js
+++ middlewares/is-authenticated.js
@@ -2,16 +2,16 @@ const { verifyToken } = require("../libraries/jwt/src");
 const { ForbiddenException } = require("../libraries/exception/exceptions");
 
 const isAuthenticated = async (req, res, next) => {
-  if (!req.cookies) {
-    throw new ForbiddenException("Cookies are required");
-  }
-
-  if (!req.cookies.accessToken) {
-    throw new ForbiddenException("Cookies are required");
-  }
-  const { accessToken } = req.cookies;
-
   try {
+    if (!req?.cookies) {
+      throw new ForbiddenException("Cookies are required");
+    }
+
+    if (!req.cookies?.accessToken) {
+      throw new ForbiddenException("Access token is required");
+    }
+    const { accessToken } = req.cookies;
+
     const data = verifyToken(accessToken, "ACCESS_TOKEN");
     if (!data) {
       throw new ForbiddenException("Access token is required");
